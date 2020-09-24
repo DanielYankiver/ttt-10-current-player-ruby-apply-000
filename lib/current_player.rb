db7fc587763c49aa4cc9482c024771055440f2c3
Daniel Yankiver <dyankiver@gmail.com>
2020-09-24 19:00:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,10 @@
 # 1. method that will tell us how many turns have been played
 # 2. method that will return, based on that information, an "X" if it is player "X"'s turn, and an "O" if it is player "O"'s turn
 
-def turn_count
+def turn_count(board_array)
 
+
+  
 end 
 
 
