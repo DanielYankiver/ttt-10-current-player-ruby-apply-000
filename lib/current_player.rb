f77a1c9cea145890bf65ee925fc26262bede4a9e
Daniel Yankiver <dyankiver@gmail.com>
2020-09-24 19:09:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@
 # 1. method that will tell us how many turns have been played
 # 2. method that will return, based on that information, an "X" if it is player "X"'s turn, and an "O" if it is player "O"'s turn
 
-def turn_count(board)
+def turn_count(board_array)
   turns = 0 
-  board.each do |space| 
+  board_array.each do |space| 
     if space == "X" || space == "O" 
       turns +=1  
     end 
